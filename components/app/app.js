d9309634e9bdebe0bb6ba1e800c9b49803281db5
Mikhail Belokoskov <mbel1221@gmail.com>
2017-10-28 20:07:40 +0300

    Begin

--- components/app/app.js
+++ components/app/app.js
@@ -2,7 +2,6 @@
     'use strict';
 
     // import
-    let Menu = window.Menu;
     let Form = window.Form;
     let Service = window.Service;
 
@@ -17,80 +16,9 @@
         constructor({el}) {
             this.el = el;
 
-            this.menu = new Menu({
-                el: document.querySelector('.js-menu'),
-                data: {
-                    title: 'Список дел в этой вселенной',
-                    items: [
-                        {
-                            day: new Date(),
-                            anchor: 'Loading...',
-                            href: 'https://duna2chat.firebaseio.com/menu/menu1808.json'
-                        }
-                    ]
-                },
-                onPick(item) {
-                    console.log(item);
-                }
-            });
-
             this.form = new Form({
-                el: el.querySelector('.js-form'),
-                data: {}
+                el: el.querySelector('.js-form')
             });
-
-            // Обрабатываем всплывающее событие с form
-            this.el.addEventListener('toChat', (event) => {
-                this.menu.addItem(event.detail);
-                this.uploadData();
-            });
-
-            // Обрабатываем click событие на слайдшоу
-            this.el.querySelector('.pics').addEventListener('click', this.getBigPic.bind(this));
-
-            this.loadData();
-        }
-
-        /**
-         * Load data from server
-         * @return {Promise<*>}
-         */
-        loadData() {
-            return Service.getItems()
-            .then((resp) => {
-                this.menu.setData(resp);
-            })
-            .catch((error) => {
-                console.log('Error fetch(loadData): ' + error.message);
-            });
-        }
-
-        /**
-         * Upload data to the server
-         * @return {Promise<*>}
-         */
-        uploadData() {
-            return Service.putItems(this.menu.data)
-            .catch((error) => {
-                console.log('Error fetch(uploadData): ' + error.message);
-            });
-        }
-
-        /**
-         * Open big picture
-         */
-        getBigPic() {
-            let pics = this.el.querySelectorAll('.map__img');
-            let target = pics[0];
-            for (let i = 0; i < pics.length; i++) {
-                let op = 0;
-                let st = getComputedStyle(pics[i]);
-                if (st.opacity > op) {
-                    op = st.opacity;
-                    target = pics[i];
-                }
-            }
-            window.open(target.src);
         }
     }
 
